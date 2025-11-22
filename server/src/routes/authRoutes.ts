720415acb11d30a10ed00658b75167ed14deaced
Kranthi Kumar Banda <bandakranthi31@gmail.com>
2025-11-22 22:05:55 +0530

    login

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -4,6 +4,44 @@ import { pool } from "../database/db";
 
 const router = Router();
 
+router.post("/login", async (req: Request , res : Response)=>{
+
+    const {username , password} = req.body;
+
+     if(!username || !password){
+    res.status(404).send({
+       status: "all fields are must",
+    })
+}
+
+const result = await pool.query(`SELECT * FROM users WHERE username = $1`,[username]);
+
+const user = result.rows[0];
+
+const isMatchPassword = await bcrypt.compare(password,user.password);
+
+
+if(isMatchPassword){
+    res.status(200).send({
+      status: "sucess",
+      message: "Login in sucessfully",
+      data: { username },
+    });
+} else{
+    res.status(404).send({
+      status: "failss",
+      message: "Loginfailkedy",
+      data: { username },
+    });
+}
+
+
+});
+
+
+
+
+
 
 router.post("/signup", async (req:Request , res: Response)=>{
     const { username,email , password} = req.body;
